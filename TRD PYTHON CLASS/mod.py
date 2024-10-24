0ff26145f80ab7a7b3359ea772672e8cafc4d88a
Sunpaul <olaojopaul64@gmail.com>
2024-10-24 12:46:18 +0100

    Data Science Materials

---
+++ TRD PYTHON CLASS/mod.py
@@ -0,0 +1,50 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 2,
+   "id": "1b0980ec-d2ce-4ff0-9984-177e2000be23",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "s = \"If Comrade Napoleon says it, it must be right.\"\n",
+    "a = [100, 200, 300]\n",
+    "\n",
+    "def foo(arg):\n",
+    "    print(f'arg = {arg}')\n",
+    "\n",
+    "class Foo:\n",
+    "    pass"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "18c83588-9a7d-45b4-ac4f-0d75a9ad3346",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.9.19"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
